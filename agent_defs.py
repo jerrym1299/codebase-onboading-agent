da4cbe6fd0827280eda57397dc05e16913f72669
Jerry Mahajan <jerrym1299@gmail.com>
2026-04-15 12:10:18 +1000

    added agent comment

--- agent_defs.py
+++ agent_defs.py
@@ -3,6 +3,7 @@ from typing import Any
 from agents import Agent
 from services.tools import list_files, search_code, read_file
 
+#Explorer agnet finds exact matches in the codebase and returns the file:line number of the matches
 explorer_agent = Agent[Any](
     name="Explorer",
     instructions="You are an assistant to explore the codebase. Your job is to find things like functions, classes, files and functionality.",
